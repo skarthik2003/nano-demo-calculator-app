553e62923134eac524df5e91b99c24734fdafae4
vinay <vinaysrikakolapu666@gmail.com>
2023-08-16 12:42:32 +0530

    test

--- server.py
+++ server.py
@@ -1,1 +1,24 @@
-print("hello")
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/calculator/greeting", methods=['GET'])
+def greeting():
+    return 'Hello world!'
+
+@app.route("/calculator/add", methods=['POST'])
+def add():
+    number = request.json
+    result = number['first'] + number['second']
+    return {"result": result}
+
+@app.route("/calculator/subtract", methods=['POST'])
+def subtract():
+    numbers = request.json
+    result = numbers['first'] - numbers['second']
+    return {"result": result}
+
+if __name__ == '__main__':
+    app.run(port=8080,host='0.0.0.0')
